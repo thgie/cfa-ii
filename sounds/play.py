57ad352ecfa2c993692c1c4c1c2e0acfce8050a1
adrian <y@thgie.ch>
2019-05-08 14:23:06 +0200

    Minor changes on play

--- sounds/play.py
+++ sounds/play.py
@@ -2,15 +2,15 @@
 
 import music
 
-music.set_tempo(ticks=100, bpm=10000)
+""" music.set_tempo(ticks=100, bpm=10000) """
 			
-for i in range(50):
+""" for i in range(50):
 	for y in range(20):
 		music.play(["C4:4", "E4:8"])
 	for z in range(20):
-		music.play(["B4:4", "D4:8"])
-
-""" music.play(music.NYAN) """
+		music.play(["B4:4", "D4:8"]) 
+"""
+music.play(music.NYAN)
 
 """ tune = ["C4:4", "D4:4", "E4:4", "C4:4", "C4:4", "D4:4", "E4:4", "C4:4",
         "E4:4", "F4:4", "G4:8", "E4:4", "F4:4", "G4:8"]
